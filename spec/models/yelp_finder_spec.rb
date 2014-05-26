5aa12cfce78fd23eb38dc64572b6c3adb2458cc7
Raphael Weiner & Shane Rogers <pair+raphael+shane@rawlabs.co>
2014-05-26 15:33:56 -0600

    stub yelp response in finder spec

---
+++ spec/models/yelp_finder_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe YelpFinder do
+  let(:client) { double('client') }
+
+  subject { YelpFinder.new(client) }
+
+  describe '#find_business' do
+    it 'returns a yelp business' do
+      yelp_business = double(
+        'yelp business',
+        rating: 2.5,
+        review_count: 12,
+        url: 'www.yelp.com/your-mover',
+        mobile_url: 'm.yelp.com/your-mover',
+        rating_img_url: 'www.example.com/image/1',
+        rating_img_url_small: 'www.example.com/image/1/small',
+        snippet_text: 'These guys were...'
+      )
+
+      allow(client).to receive(:business).with('yelp-san-francisco').and_return(yelp_business)
+
+      business = subject.find_business('yelp-san-francisco')
+
+      expect(business.rating).to eq 2.5
+      expect(business.review_count).to eq 12
+      expect(business.url).to eq 'www.yelp.com/your-mover'
+      expect(business.mobile_url).to eq 'm.yelp.com/your-mover'
+      expect(business.rating_img_url).to eq 'www.example.com/image/1'
+      expect(business.rating_img_url_small).to eq 'www.example.com/image/1/small'
+      expect(business.snippet_text).to eq 'These guys were...'
+    end
+
+    it 'returns nil if not found' do
+      allow(client).to receive(:business).with('unknown-business-id').and_raise(JSON::ParserError)
+
+      expect(subject.find_business('unknown-business-id')).to eq nil
+    end
+  end
+end
