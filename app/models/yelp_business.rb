9c4c8c92b21e3e180ccfd2c01f1745d9071c8b08
Raphael Weiner <pair+raphael@rawlabs.co>
2014-06-01 22:48:02 -0600

    YelpBusiness returns 5 instead of 5.0 (css ratings does not recognize 5.0)

--- app/models/yelp_business.rb
+++ app/models/yelp_business.rb
@@ -1,8 +1,17 @@
 class YelpBusiness
-  attr_accessor :stars,
-                :review_count,
+  attr_accessor :review_count,
                 :review_snippet,
                 :url,
                 :stars_img_url,
                 :stars_img_url_small
+
+  attr_writer :stars
+
+  def stars
+    if @stars == 5.0
+      5
+    else
+      @stars
+    end
+  end
 end
---
+++ spec/models/yelp_business_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe YelpBusiness do
+  describe '#stars' do
+    it 'return 5 if @stars is 5.0' do
+      subject.stars = 5.0
+
+      expect(subject.stars).to eq 5
+    end
+
+    it 'acts normally otherwise' do
+      subject.stars = 4.5
+
+      expect(subject.stars).to eq 4.5
+    end
+  end
+end
