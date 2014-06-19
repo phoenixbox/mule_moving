64a8379d63f3fb812205ded4f8d6944d5b0850ed
Raphael Weiner <pair+raphael@rawlabs.co>
2014-06-18 21:19:56 -0600

    add some simple styles and improve booking page

--- spec/acceptance/booking_spec.rb
+++ spec/acceptance/booking_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "Booking", type: :feature, js: true do
-  it "can be done on index page" do
+describe "Booking", type: :feature do
+  it "can be done on index page", js: true do
     create_mover name: "Mafioso Movers"
 
     visit movers_path(from: "Boulder", to: "Denver")
@@ -11,8 +11,24 @@ describe "Booking", type: :feature, js: true do
     expect(page).to have_field :booking_from, with: "Boulder"
     expect(page).to have_field :booking_to, with: "Denver"
 
-    page.execute_script "$('td.day:contains(12)').click()"
+    fill_in :booking_move_date, with: "06/26/2014"
+    fill_in :booking_email, with: "me@example.com"
 
+    click_on "Book"
+
+    expect(page).to have_content "Success"
+  end
+
+  it "can be done on the details page" do
+    mafioso = create_mover name: "Mafioso Movers"
+
+    visit mover_path(mafioso.id)
+
+    click_on "Book"
+
+    fill_in :booking_from, with: "Boulder"
+    fill_in :booking_to, with: "Denver"
+    fill_in :booking_move_date, with: "06/26/2014"
     fill_in :booking_email, with: "me@example.com"
 
     click_on "Book"
