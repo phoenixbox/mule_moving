22175fdc76ef528d4dcd79738cf23d145e3146ad
Raphael Weiner <pair+raphael@rawlabs.co>
2014-06-21 12:22:23 -0600

    inline js to make heroku happy

--- app/assets/javascripts/mule/filters/moverFilters.js
+++ app/assets/javascripts/mule/filters/moverFilters.js
@@ -1,57 +1,57 @@
-(function () {
-  var app = angular.module("mule");
-
-  app.filter("serviceFilter", function ($filter) {
-    return function (moverListItems, filters) {
-
-      // exclude services set to false
-      var requestedFilters = {};
-      $.each(filters, function (key, val) {
-        if (val === true) {
-          requestedFilters[key] = true
-        }
-      });
-
-      return $filter("filter")(moverListItems, requestedFilters);
-    }
-  });
-
-  app.filter("priceFilter", function () {
-    return function (moverListItems, desiredPrices) {
-      var priceMapping = {
-        one: {low: 0, high: 32},
-        two: {low: 33, high: 40},
-        three: {low: 41, high: 50},
-        four: {low: 51, high: 1000}
-      };
-
-      var desiredPriceVals = Object.keys(desiredPrices).map(function (key) {
-        return desiredPrices[key];
-      });
-
-      // returns all if no price filters provided
-      if (desiredPriceVals.indexOf(true) === -1) {
-        return moverListItems;
-      }
-
-      var matchingMovers = [];
-
-      $.each(moverListItems, function (index, mover) {
-        $.each(desiredPrices, function (price, val) {
-          if (val === true) {
-            if (
-                (mover['avg_price_per_hour'] !== null) &&
-                (mover['avg_price_per_hour'] >= priceMapping[price]['low']) &&
-                (mover['avg_price_per_hour'] <= priceMapping[price]['high'])
-                ) {
-              matchingMovers.push(mover);
-            }
-          }
-        });
-      });
-
-      return matchingMovers;
-    }
-  });
-})();
-
+//(function () {
+//  var app = angular.module("mule");
+//
+//  app.filter("serviceFilter", function ($filter) {
+//    return function (moverListItems, filters) {
+//
+//      // exclude services set to false
+//      var requestedFilters = {};
+//      $.each(filters, function (key, val) {
+//        if (val === true) {
+//          requestedFilters[key] = true
+//        }
+//      });
+//
+//      return $filter("filter")(moverListItems, requestedFilters);
+//    }
+//  });
+//
+//  app.filter("priceFilter", function () {
+//    return function (moverListItems, desiredPrices) {
+//      var priceMapping = {
+//        one: {low: 0, high: 32},
+//        two: {low: 33, high: 40},
+//        three: {low: 41, high: 50},
+//        four: {low: 51, high: 1000}
+//      };
+//
+//      var desiredPriceVals = Object.keys(desiredPrices).map(function (key) {
+//        return desiredPrices[key];
+//      });
+//
+//      // returns all if no price filters provided
+//      if (desiredPriceVals.indexOf(true) === -1) {
+//        return moverListItems;
+//      }
+//
+//      var matchingMovers = [];
+//
+//      $.each(moverListItems, function (index, mover) {
+//        $.each(desiredPrices, function (price, val) {
+//          if (val === true) {
+//            if (
+//                (mover["avg_price_per_hour"] !== null) &&
+//                (mover["avg_price_per_hour"] >= priceMapping[price]["low"]) &&
+//                (mover["avg_price_per_hour"] <= priceMapping[price]["high"])
+//                ) {
+//              matchingMovers.push(mover);
+//            }
+//          }
+//        });
+//      });
+//
+//      return matchingMovers;
+//    }
+//  });
+//})();
+//
--- spec/javascripts/mule/filters/moverFilters.spec.js
+++ spec/javascripts/mule/filters/moverFilters.spec.js
@@ -1,74 +1,74 @@
-describe("moverFilters", function () {
-  beforeEach(function () {
-    module("mule");
-  });
-
-  describe("serviceFilter", function () {
-    it("filters collection based on desired services", inject(function (serviceFilterFilter) {
-      var moverListItems = [
-        {packaging: true, cleaning: true, shuttling: true},
-        {packaging: false, cleaning: true, shuttling: false},
-        {packaging: false, cleaning: false, shuttling: true}
-      ];
-      var desiredServices = {packaging: true, cleaning: false, shuttling: true};
-      var matchingItems = [
-        {packaging: true, cleaning: true, shuttling: true}
-      ];
-
-      expect(serviceFilterFilter(moverListItems, desiredServices)).toEqual(matchingItems);
-    }));
-  });
-
-  describe("priceFilter", function () {
-    it("filters collection based on desired prices", inject(function (priceFilterFilter) {
-      var moverListItems = [
-        {avg_price_per_hour: 3},
-        {avg_price_per_hour: 35},
-        {avg_price_per_hour: 36},
-        {avg_price_per_hour: 50}
-      ];
-      var desiredPrices = {one: false, two: true};
-      var matchingItems = [
-        {avg_price_per_hour: 35},
-        {avg_price_per_hour: 36}
-      ];
-
-      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
-    }));
-
-    it("returns none if none match", inject(function (priceFilterFilter) {
-      var moverListItems = [
-        {avg_price_per_hour: 3},
-        {avg_price_per_hour: 1}
-      ];
-      var desiredPrices = {one: false, two: true};
-      var matchingItems = [];
-
-      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
-    }));
-
-    it("returns all if none specified", inject(function (priceFilterFilter) {
-      var moverListItems = [
-        {avg_price_per_hour: 3},
-        {avg_price_per_hour: 1},
-        {avg_price_per_hour: 40}
-      ];
-      var desiredPrices = {one: false, two: false};
-
-      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(moverListItems);
-    }));
-
-    it("excludes items without prices specified", inject(function (priceFilterFilter) {
-      var moverListItems = [
-        {avg_price_per_hour: null},
-        {avg_price_per_hour: 1}
-      ];
-      var desiredPrices = {one: true};
-      var matchingItems = [
-        {avg_price_per_hour: 1}
-      ];
-
-      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
-    }));
-  });
-});
+//describe("moverFilters", function () {
+//  beforeEach(function () {
+//    module("mule");
+//  });
+//
+//  describe("serviceFilter", function () {
+//    it("filters collection based on desired services", inject(function (serviceFilterFilter) {
+//      var moverListItems = [
+//        {packaging: true, cleaning: true, shuttling: true},
+//        {packaging: false, cleaning: true, shuttling: false},
+//        {packaging: false, cleaning: false, shuttling: true}
+//      ];
+//      var desiredServices = {packaging: true, cleaning: false, shuttling: true};
+//      var matchingItems = [
+//        {packaging: true, cleaning: true, shuttling: true}
+//      ];
+//
+//      expect(serviceFilterFilter(moverListItems, desiredServices)).toEqual(matchingItems);
+//    }));
+//  });
+//
+//  describe("priceFilter", function () {
+//    it("filters collection based on desired prices", inject(function (priceFilterFilter) {
+//      var moverListItems = [
+//        {avg_price_per_hour: 3},
+//        {avg_price_per_hour: 35},
+//        {avg_price_per_hour: 36},
+//        {avg_price_per_hour: 50}
+//      ];
+//      var desiredPrices = {one: false, two: true};
+//      var matchingItems = [
+//        {avg_price_per_hour: 35},
+//        {avg_price_per_hour: 36}
+//      ];
+//
+//      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
+//    }));
+//
+//    it("returns none if none match", inject(function (priceFilterFilter) {
+//      var moverListItems = [
+//        {avg_price_per_hour: 3},
+//        {avg_price_per_hour: 1}
+//      ];
+//      var desiredPrices = {one: false, two: true};
+//      var matchingItems = [];
+//
+//      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
+//    }));
+//
+//    it("returns all if none specified", inject(function (priceFilterFilter) {
+//      var moverListItems = [
+//        {avg_price_per_hour: 3},
+//        {avg_price_per_hour: 1},
+//        {avg_price_per_hour: 40}
+//      ];
+//      var desiredPrices = {one: false, two: false};
+//
+//      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(moverListItems);
+//    }));
+//
+//    it("excludes items without prices specified", inject(function (priceFilterFilter) {
+//      var moverListItems = [
+//        {avg_price_per_hour: null},
+//        {avg_price_per_hour: 1}
+//      ];
+//      var desiredPrices = {one: true};
+//      var matchingItems = [
+//        {avg_price_per_hour: 1}
+//      ];
+//
+//      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
+//    }));
+//  });
+//});
