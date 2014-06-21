a4e1ce250d6685db27e24fc83a275764bb4da95a
Raphael Weiner <pair+raphael@rawlabs.co>
2014-06-21 12:04:35 -0600

    add price filter to movers index and tweak definition

--- spec/javascripts/mule/filters/moverFilters.spec.js
+++ spec/javascripts/mule/filters/moverFilters.spec.js
@@ -23,14 +23,14 @@ describe("moverFilters", function () {
     it("filters collection based on desired prices", inject(function (priceFilterFilter) {
       var moverListItems = [
         {avg_price_per_hour: 3},
-        {avg_price_per_hour: 15},
-        {avg_price_per_hour: 16},
-        {avg_price_per_hour: 21}
+        {avg_price_per_hour: 35},
+        {avg_price_per_hour: 36},
+        {avg_price_per_hour: 50}
       ];
       var desiredPrices = {one: false, two: true};
       var matchingItems = [
-        {avg_price_per_hour: 15},
-        {avg_price_per_hour: 16}
+        {avg_price_per_hour: 35},
+        {avg_price_per_hour: 36}
       ];
 
       expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
@@ -43,6 +43,30 @@ describe("moverFilters", function () {
       ];
       var desiredPrices = {one: false, two: true};
       var matchingItems = [];
+
+      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
+    }));
+
+    it("returns all if none specified", inject(function (priceFilterFilter) {
+      var moverListItems = [
+        {avg_price_per_hour: 3},
+        {avg_price_per_hour: 1},
+        {avg_price_per_hour: 40}
+      ];
+      var desiredPrices = {one: false, two: false};
+
+      expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(moverListItems);
+    }));
+
+    it("excludes items without prices specified", inject(function (priceFilterFilter) {
+      var moverListItems = [
+        {avg_price_per_hour: null},
+        {avg_price_per_hour: 1}
+      ];
+      var desiredPrices = {one: true};
+      var matchingItems = [
+        {avg_price_per_hour: 1}
+      ];
 
       expect(priceFilterFilter(moverListItems, desiredPrices)).toEqual(matchingItems);
     }));
